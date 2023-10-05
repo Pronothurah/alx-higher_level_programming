aee49a596f0841baa076ee991476b1049c428885
Pronothurah <onsongojosphat28@gmail.com>
2023-10-05 22:35:47 +0300

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -10,4 +10,4 @@ if __name__ == "__main__":
     else:
         print("{} arguments:".format(count))
     for i in range(count):
-        print(f"{}: {}".format(i + 1, sys.argv[i + 1]))
+        print(f"{i + 1}: {sys.argv[i + 1]}")
