4c3aeb03da7d88e246acbad108cb065e9bff71cf
Pronothurah <onsongojosphat28@gmail.com>
2023-10-09 12:08:34 +0300

    1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
+    """
+    Retrieves an element from a list.
+    Args:
+        my_list: A list of integers to be printed.
+        idx: index to be retrieved
+    Returns:
+        None or element at index
+    """
     if idx < 0 or idx >= len(my_list):
         return None
     return my_list[idx]
-
