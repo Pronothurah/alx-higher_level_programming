f924d54f6ec3bb7fe335a595a64cb92b0065829e
Pronothurah <onsongojosphat28@gmail.com>
2023-10-24 15:06:44 +0300

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Square Module"""
+
+
+class Square:
+    """Defines square"""
+
+    def __init__(self, size):
+        """
+        Constructor
+        Args:
+            size: length of one side
+        """
+
+        self.__size = size
