c6359bdd82e91f139c7ae57409172b22b7c19e2f
Pronothurah <onsongojosphat28@gmail.com>
2023-10-24 13:27:41 +0300

    4-list_division.py

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -4,13 +4,10 @@ def list_division(my_list_1, my_list_2, list_length):
 
     for i in range(list_length):
         try:
-            numerator = float(my_list_1[i])
-            denominator = float(my_list_2[i])
-            if denominator == 0:
-                result.append(0)
-                print("division by 0")
-            else:
-                result.append(numerator / denominator)
+            result.append(my_list_1[i] / my_list_2[i])
+        except ZeroDivisionError:
+            result.append(0)
+            print("division by 0")
         except IndexError:
             result.append(0)
             print("out of range")
