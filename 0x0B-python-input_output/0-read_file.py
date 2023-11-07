f754572630aeff52baed38efafe694ed6c881b63
Pronothurah <onsongojosphat28@gmail.com>
2023-11-07 20:14:32 +0300

    0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module for read_file method"""
+
+
+def read_file(filename=""):
+    """reads a text file (UTF8) and prints it to stdout:"""
+    with open(filename, 'r', encoding='utf-8') as a_file:
+        content = a_file.read()
+        print(content)
