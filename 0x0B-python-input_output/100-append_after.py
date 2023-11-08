832ee90a90f4ca474731c484995199f1e595b8e2
Pronothurah <onsongojosphat28@gmail.com>
2023-11-08 14:01:34 +0300

    100-append_after.py

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -3,15 +3,15 @@
 
 
 def append_after(filename="", search_string="", new_string=""):
-    """ 
+    """
     inserts a line of text to a file, after
     each line containing a specific string
-    
+
     Attributes:
         filename (:obj:`str`, optional): name of the file
         search_string (:obj:`str`, optional): string to look for
         new_string (:obj:`str`, optional): string to append
-    
+
     """
     line_list = []
 
