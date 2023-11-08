2bcc2d42f304dbb3db67d7246b7312b284e47a3c
Pronothurah <onsongojosphat28@gmail.com>
2023-11-08 11:29:10 +0300

    7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,31 +1,26 @@
 #!/usr/bin/python3
-"""Module script that adds all arguments to a Python list,
-and then save them to a file:
-"""
-
+"""A script that adds all arguments to a Python list,
+and then save them to a file:"""
 
 import sys
-import json
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-
-
-def add_arguments_to_list(arguments, filename):
-    try:
-        initial_data = load_from_json_file(filename)
-    except FileNotFoundError:
-        initial_data = []
-
-    initial_data.extend(arguments)
-    save_to_json_file(initial_data, filename)
-
 
 if __name__ == "__main__":
-    arguments = sys.argv[1:]
+    """Save json import"""
 
-    if len(arguments) == 0:
-        print("Usage: python script.py argument1 argument2 ...")
-    else:
-        filename = 'add_item.json'
-        add_arguments_to_list(arguments, filename)
-        print(f"Arguments added to {filename}: {arguments}")
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+    """Load json import"""
+
+    load_from_json_file = __import__(
+        '6-load_from_json_file').load_from_json_file
+
+    filename = 'add_item.json'
+    arguments = []
+
+    try:
+        arguments = load_from_json_file(filename)
+    except FileNotFoundError:
+        arguments = []
+
+    arguments.extend(sys.argv[1:])
+    save_to_json_file(arguments, filename)
