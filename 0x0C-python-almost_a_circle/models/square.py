8e2dee029fb76345ec533ad6b3157137a5888141
Pronothurah <onsongojosphat28@gmail.com>
2023-11-13 18:21:32 +0300

    add-square-attributes

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -10,7 +10,7 @@ class Square(Rectangle):
         """Constructor"""
 
         super().__init__(size, size, x, y, id)
-    
+
     def __str__(self):
         """
         returns string info about the square
@@ -23,3 +23,32 @@ class Square(Rectangle):
             self.y,
             self.width
             )
+
+    @property
+    def size(self):
+        """defines size of square"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
+
+    def update(self, *args, **kargs):
+        """
+        Update the square attributes based on
+        *args (non-keyword arguments) or **kwargs (keyword arguments)
+        """
+
+        if (args is not None and len(args) > 0):
+            i = 0
+            attributes = ["id", "size", "x", "y"]
+            for arg in args:
+                try:
+                    setattr(self, attributes[i], arg)
+                    i += 1
+                except IndexError:
+                    pass
+        else:
+            for k, v in kargs.items():
+                setattr(self, k, v)
