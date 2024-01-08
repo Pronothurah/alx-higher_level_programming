da1077ed69479c85fe5f93277af6f0a341a13a81
Pronothurah <onsongojosphat28@gmail.com>
2024-01-08 19:47:02 +0300

    1-multi_languages.js

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+// Create a variables to store the data
+let langC = 'C is fun';
+let langP = 'Python is cool';
+let langJ = 'JavaScript is amazing';
+
+// Use console.log(...) to print the output
+console.log(langC);
+console.log(langP);
+console.log(langJ);
