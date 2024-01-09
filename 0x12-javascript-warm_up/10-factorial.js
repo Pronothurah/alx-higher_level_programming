05b1678e3de45bad2feb4d16cfdcea298d9eff9f
Pronothurah <onsongojosphat28@gmail.com>
2024-01-09 12:07:55 +0300

    semistandard fixes

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -3,7 +3,7 @@
 function computeFactorial (n) {
   if (isNaN(n)) {
     return 1;
-  } else if (n == 0 || n == 1) {
+  } else if (n === 0 || n === 1) {
     return 1;
   } else {
     return n * computeFactorial(n - 1);
