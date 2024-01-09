6c6a6eb15b0a84497fc0b3302219f61879f5eebb
Pronothurah <onsongojosphat28@gmail.com>
2024-01-09 16:23:50 +0300

    101-sorted.js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,2 +1,15 @@
 #!/usr/bin/node
 
+const dict = require('./101-data.js').dict;
+
+const newDict = {};
+
+Object.getOwnPropertyNames(dict).forEach(occurrences => {
+  if (newDict[dict[occurrences]] === undefined) {
+    newDict[dict[occurrences]] = [occurrences];
+  } else {
+    newDict[dict[occurrences]].push(occurrences);
+  }
+});
+
+console.log(newDict);
