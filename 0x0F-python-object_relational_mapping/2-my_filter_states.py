cb66dcae76694f73c6e1040280066d242c4da5ad
Pronothurah <onsongojosphat28@gmail.com>
2024-01-13 12:25:53 +0300

    fixes

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,46 +1,29 @@
 #!/usr/bin/python3
 """
-This module is about listing all states with a name starting
-with N (upper N) from the database hbtn_0e_0_usa
+This module is about taking in an argument and displays all values
+in the states table of hbtn_0e_0_usa where name matches the argument
 """
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    if len(sys.argv) != 5:
-        print("Usage: python script.py <username> <password> <database> <state_name>")
-        sys.exit(1)
-
-    username, password, database, state_name = sys.argv[1:]
-
-    try:
-        # Connect to MySQL server
-        db_connection = MySQLdb.connect(
-            host='localhost',
-            port=3306,
-            user=username,
-            passwd=password,
-            db=database
-        )
-
-        # Create a cursor object to execute queries
-        cursor = db_connection.cursor()
-
-        # Use format to create the SQL query with the user input
-        query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(state_name)
-
-        # Execute the query
-        cursor.execute(query)
-
-        # Fetch and print the results
-        results = cursor.fetchall()
-        for result in results:
-            print(result)
-
-    except MySQLdb.Error as e:
-        print("Error connecting to MySQL:", e)
-
-    finally:
-        # Close the cursor and database connection
-        cursor.close()
-        db_connection.close()
+    args = sys.argv[1:]
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=args[0],
+        password=args[1],
+        database=args[2]
+    )
+    cur = db.cursor()
+    query = """
+    SELECT * FROM states
+    WHERE BINARY `name`='{query}'
+    ORDER BY id ASC
+    """
+    cur.execute(query.format(query=args[3]))
+    for data in cur.fetchall():
+        print(data)
+    cur.close()
+    db.close()
+    
