968db1beec69346614e07a82bb98afbbc14c19e8
Pronothurah <onsongojosphat28@gmail.com>
2024-01-13 12:23:03 +0300

    4-cities_by_state.py

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,30 +1,26 @@
 #!/usr/bin/python3
-"""
-This module is about listing all cities from the database hbtn_0e_4_usa
-"""
+'''Prints all cities and their state in a database.
+'''
 import sys
 import MySQLdb
 
-if __name__ == "__main__":
-    args = sys.argv[1:4]
-    if len(args) == 3:
-        db = MySQLdb.connect(
-            host="localhost",
+
+if __name__ == '__main__':
+    if len(sys.argv) >= 4:
+        db_connection = MySQLdb.connect(
+            host='localhost',
             port=3306,
-            user=args[0],
-            password=args[1],
-            database=args[2]
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3]
         )
-        cur = db.cursor()
-        cur.execute(
-            """
-            SELECT cities.id, cities.name, states.name FROM cities
-            INNER JOIN states
-            ON cities.state_id = states.id
-            ORDER BY cities.id ASC
-            """
+        cursor = db_connection.cursor()
+        cursor.execute(
+            'SELECT cities.id, cities.name, states.name FROM cities' +
+            ' INNER JOIN states ON cities.state_id = states.id' +
+            ' ORDER BY cities.id ASC;'
         )
-        for data in cur.fetchall():
-            print(data)
-        cur.close()
-        db.close()
+        results = cursor.fetchall()
+        for result in results:
+            print(result)
+        db_connection.close()
