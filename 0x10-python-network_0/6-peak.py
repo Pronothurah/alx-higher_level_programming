3d111f486ebe4de9ba7897e42a4868cc1e86928b
Pronothurah <onsongojosphat28@gmail.com>
2024-01-25 19:01:19 +0300

    6-peak.py

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Module that defines the peak method"""
+
+
+def find_peak(list_of_integers):
+    """
+    Finds a peak in a list of unsorted integers.
+
+    Args:
+    - list_of_integers: List of unsorted integers.
+
+    Returns:
+    - The peak element in the list.
+    """
+    if not list_of_integers:
+        return None
+
+    low, high = 0, len(list_of_integers) - 1
+
+    while low < high:
+        mid = (low + high) // 2
+        if list_of_integers[mid] > list_of_integers[mid + 1]:
+            high = mid
+        else:
+            low = mid + 1
+
+    return list_of_integers[low]
