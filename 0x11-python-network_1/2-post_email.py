0d57584c17a11a4fe32ddf574fef9eed544511d3
Pronothurah <onsongojosphat28@gmail.com>
2024-01-26 12:27:49 +0300

    2-post_email.py

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import urllib.request
+import urllib.parse
+import sys
+
+if len(sys.argv) != 3:
+    sys.exit()
+
+url = sys.argv[1]
+email = sys.argv[2]
+
+data = urllib.parse.urlencode({'email': email}).encode('ascii')
+
+with urllib.request.urlopen(url, data) as response:
+    body = response.read()
+    print(body.decode('utf-8'))
