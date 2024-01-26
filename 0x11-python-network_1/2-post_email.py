cdf808802c2b25fb6add2364f88a4fe0c1dc66fb
Pronothurah <onsongojosphat28@gmail.com>
2024-01-26 13:31:47 +0300

    fix: fixed tasks 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
+"""Sends for data to a url"""
 import urllib.request
 import urllib.parse
 import sys
 
-if len(sys.argv) != 3:
-    sys.exit()
-
-url = sys.argv[1]
-email = sys.argv[2]
-
-data = urllib.parse.urlencode({'email': email}).encode('ascii')
-
-with urllib.request.urlopen(url, data) as response:
-    body = response.read()
-    print(body.decode('utf-8'))
+if len(sys.argv) > 2:
+    url = sys.argv[1]
+    email = sys.argv[2]
+    data = urllib.parse.urlencode({'email': email}).encode('ascii')
+    with urllib.request.urlopen(url, data) as response:
+        body = response.read()
+        print(body.decode('utf-8'))
