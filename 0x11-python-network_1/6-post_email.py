c409e5b04d2ef5c0cc2c998928a0f8e9bb036205
Pronothurah <onsongojosphat28@gmail.com>
2024-01-26 13:54:00 +0300

    fix: task 6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
+"""Sends for data to a url"""
 import requests
 import sys
 
-if len(sys.argv) != 3:
-    sys.exit()
 
-url = sys.argv[1]
-email = sys.argv[2]
+if __name__ == '__main__':
+    if len(sys.argv) > 2:
+        url = sys.argv[1]
+        email = sys.argv[2]
 
-data = {'email': email}
+        data = {'email': email}
 
-response = requests.post(url, data=data)
+        response = requests.post(url, data=data)
 
-print(response.text)
+        print(response.text)
