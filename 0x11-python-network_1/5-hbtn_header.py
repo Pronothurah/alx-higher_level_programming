5c5e1f4b6ae54ee8231aa89b6c91703cd6f50495
Pronothurah <onsongojosphat28@gmail.com>
2024-01-26 13:44:35 +0300

    5-hbtn_header.py

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -2,14 +2,11 @@
 import requests
 import sys
 
-if len(sys.argv) != 2:
-    sys.exit()
 
-url = sys.argv[1]
-
-response = requests.get(url)
-
-request_id = response.headers.get('X-Request-Id')
-
-if request_id is not None:
-    print(request_id)
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        url = sys.argv[1]
+        response = requests.get(url)
+        request_id = response.headers.get('X-Request-Id')
+        if request_id is not None:
+            print(request_id)
