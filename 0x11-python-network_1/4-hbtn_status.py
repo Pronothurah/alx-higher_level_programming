7fa903cff0c061ce1fc4a5889cfd03d39a591e21
Pronothurah <onsongojosphat28@gmail.com>
2024-01-26 13:42:41 +0300

    fix: fix for 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
+"""Fetches URL"""
 import requests
 
-url = 'https://alx-intranet.hbtn.io/status'
 
-response = requests.get(url)
-
-print("Body response:")
-print("\t- type:", type(response.text))
-print("\t- content:", response.text)
+if __name__ == '__main__':
+    url = 'https://alx-intranet.hbtn.io/status'
+    response = requests.get(url)
+    print("Body response:")
+    print("\t- type:", type(response.text))
+    print("\t- content:", response.text)
