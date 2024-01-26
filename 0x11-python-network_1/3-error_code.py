882a376be772ad22f5e6042b409582beb9dc1350
Pronothurah <onsongojosphat28@gmail.com>
2024-01-26 13:38:58 +0300

    fix: fixed tasks 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
+"""Sends a request to a URL and prints its response or error code."""
 import urllib.request
 import urllib.error
 import sys
 
-if len(sys.argv) > 1:
-    url = sys.argv[1]
 
-    try:
-        with urllib.request.urlopen(url) as response:
-            body = response.read()
-            print(body.decode('utf-8'))
-    except urllib.error.HTTPError as e:
-        print("Error code: {}".format(e.code))
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        url = sys.argv[1]
+
+        try:
+            with urllib.request.urlopen(url) as response:
+                body = response.read()
+                print(body.decode('utf-8'))
+        except urllib.error.HTTPError as e:
+            print("Error code: {}".format(e.code))
