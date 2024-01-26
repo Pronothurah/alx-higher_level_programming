e4493c951c62874b7ad52a004fdcceb227654434
Pronothurah <onsongojosphat28@gmail.com>
2024-01-26 13:09:39 +0300

    10-my_github.py

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import requests
+import sys
+
+if len(sys.argv) != 3:
+    sys.exit()
+
+username = sys.argv[1]
+token = sys.argv[2]
+
+url = 'https://api.github.com/user'
+
+response = requests.get(url, auth=(username, token))
+
+try:
+    data = response.json()
+    print(data.get('id', 'None'))
+except ValueError:
+    print('None')
