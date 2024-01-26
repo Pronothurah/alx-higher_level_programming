9ba977e993ee36035ce5b41cbe6b598189f405a6
Pronothurah <onsongojosphat28@gmail.com>
2024-01-26 13:22:23 +0300

    fix: fixed tasks 0 and 1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Fetches a URL."""
 import urllib.request
 
 url = 'https://alx-intranet.hbtn.io/status'
--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
+"""Fetches a header of a response from a URL."""
 import urllib.request
 import sys
 
-if len(sys.argv) != 2:
-    sys.exit()
+if len(sys.argv) > 1:
+    url = sys.argv[1]
 
-url = sys.argv[1]
-
-with urllib.request.urlopen(url) as response:
-    headers = response.info()
-    request_id = headers.get('X-Request-Id')
-    print(request_id)
+    with urllib.request.urlopen(url) as response:
+        print(response.headers['X-Request-Id'])
