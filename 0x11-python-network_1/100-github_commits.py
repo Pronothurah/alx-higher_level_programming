2da8320db2704b051906a0cf0f7d25480b0e8376
Pronothurah <onsongojosphat28@gmail.com>
2024-01-26 14:22:48 +0300

    fixes: 11

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -3,20 +3,24 @@
 import requests
 import sys
 
-if len(sys.argv) != 3:
-    sys.exit()
 
-repo_name = sys.argv[1]
-owner_name = sys.argv[2]
-url = f'https://api.github.com/repos/{owner_name}/{repo_name}/commits'
+if __name__ == '__main__':
+    if len(sys.argv) > 2:
+        repo_name = sys.argv[1]
+        owner_name = sys.argv[2]
+        url = f'https://api.github.com/repos/{owner_name}/{repo_name}/commits'
 
-response = requests.get(url)
+        try:
+            response = requests.get(url)
+            response.raise_for_status()  # Raise an HTTPError for bad responses (4xx and 5xx)
 
-try:
-    commits = response.json()[:10]
-    for commit in commits:
-        sha = commit['sha']
-        author_name = commit['commit']['author']['name']
-        print(f"{sha}: {author_name}")
-except ValueError:
-    print('Error retrieving commits.')
+            commits = response.json()[:10]
+            for commit in commits:
+                sha = commit['sha']
+                author_name = commit['commit']['author']['name']
+                print(f"{sha}: {author_name}")
+
+        except requests.exceptions.RequestException as e:
+            print('Error:', e)
+        except ValueError:
+            print('Error retrieving commits.')
