8535f31a507af685d7fb6631b16f09d30e044c83
Pronothurah <onsongojosphat28@gmail.com>
2024-01-26 14:02:38 +0300

    header fixes

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -8,6 +8,7 @@ if __name__ == '__main__':
     if len(sys.argv) > 1:
         url = sys.argv[1]
         response = requests.get(url)
-        print(response.text)
         if response.status_code >= 400:
             print("Error code:", response.status_code)
+        else:
+            print(response.text)
