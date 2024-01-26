aec3783e1c85d9121a952ac00258e176e5af3c38
Pronothurah <onsongojosphat28@gmail.com>
2024-01-26 13:14:44 +0300

    100-github_commits.py

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import requests
+import sys
+
+if len(sys.argv) != 3:
+    sys.exit()
+
+repo_name = sys.argv[1]
+owner_name = sys.argv[2]
+url = f'https://api.github.com/repos/{owner_name}/{repo_name}/commits'
+
+response = requests.get(url)
+
+try:
+    commits = response.json()[:10]
+    for commit in commits:
+        sha = commit['sha']
+        author_name = commit['commit']['author']['name']
+        print(f"{sha}: {author_name}")
+except ValueError:
+    print('Error retrieving commits.')
